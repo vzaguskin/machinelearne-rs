f64689a05fd63cf81249af5aff3c428d856925ee
Victor Zaguskin <victor.zaguskin@gmail.com>
2026-01-04 16:41:41 +0300

    L2 regularizer support

---
+++ examples/train_linear_l2.rs
@@ -0,0 +1,25 @@
+// examples/train_linear.rs или в тестах
+use machinelearne_rs::{
+    loss::MSELoss, model::linear::LinearRegressor, optimizer::SGD, regularizers::L2, trainer::Trainer
+};
+
+fn main() {
+
+    let model = LinearRegressor::new(2); // 2 фичи
+    let loss = MSELoss;
+    let opt = SGD::new(0.01);
+    let reg = L2::new(0.1);
+    let trainer = Trainer::builder(loss, opt, reg)
+        .batch_size(20)
+        .max_epochs(500)
+        .build();
+    let x = vec![
+        vec![1.0, 2.0],
+        vec![2.0, 3.0],
+        vec![3.0, 4.0],
+    ];
+    let y = vec![3.0, 5.0, 7.0];
+
+    let fitted_model = trainer.fit(model, &x, &y).unwrap();
+    println!("Prediction: {}", fitted_model.predict(&[4.0, 5.0]));
+}
